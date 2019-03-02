7045f6287a21843d17a2916810b0cf079f501b3a
DubstepWar <android6636@gmail.com>
2019-03-02 13:40:44 +0200

    poblem with JS file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -15,11 +16,15 @@
                         <a href="/korresp_chart">KORRESP</a>
                     </button>
 
-
-
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<canvas id="myChart"></canvas>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.js" charset="utf-8"></script>
+<script src="/public/js/JSchart.js"></script>
+
+
 @endsection
